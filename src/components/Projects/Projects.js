a595ffbe1d45c6b5b98fdbcbcf01c7ae8c7b90ed
suchinkumar <sk263@snu.edu.in>
2024-07-17 06:19:30 +0530

    Initial commit

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,12 +2,12 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import leaf from "../../Assets/Projects/leaf.png";
-import emotion from "../../Assets/Projects/emotion.png";
-import editor from "../../Assets/Projects/codeEditor.png";
-import chatify from "../../Assets/Projects/chatify.png";
+import leaf from "../../Assets/Projects/GeeksForum.webp";
+import emotion from "../../Assets/Projects/MarkovNetSimPro.webp";
+import editor from "../../Assets/Projects/CampusTradeHub.webp";
+import chatify from "../../Assets/Projects/GradeX.webp";
 import suicide from "../../Assets/Projects/suicide.png";
-import bitsOfCode from "../../Assets/Projects/blog.png";
+import bitsOfCode from "../../Assets/Projects/AppTrackPro.webp";
 
 function Projects() {
   return (
